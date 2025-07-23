97ee752ffa88ed3d88c0dc2e864335d38a7a6448
arihant999 <arihantofficial999@gmail.com>
2025-07-23 18:48:58 +0530

    Add proxy code

--- api/proxy.js
+++ api/proxy.js
@@ -1,16 +1,24 @@
-import fetch from 'node-fetch';
+const https = require('https');
 
-export default async (req, res) => {
-  const resp = await fetch('https://studyuk.fun/goal/');
-  let html = await resp.text();
+export default function handler(req, res) {
+  https.get('https://studyuk.fun/goal/', (response) => {
+    let data = '';
 
-  html = html
-    .replace(/studyuk\.fun/gi, 'nakoda-bhairav-clone.vercel.app')
-    .replace(/@SDV_BOTX/g, '@MockManager')
-    .replace(/https?:\/\/t\\.me\/[^"']+/g, 'https://t.me/+rc5Psv_S2VJkMGM1')
-    .replace(/StudyUK/gi, 'NAKODA BHAIRAV');
+    response.on('data', chunk => { data += chunk; });
+    response.on('end', () => {
+      let html = data
+        .replace(/studyuk\.fun/g, 'nakoda-bhairav-clone.vercel.app')
+        .replace(/@SDV_BOTX/g, '@MockManager')
+        .replace(/https:\/\/t\.me\/[^"']+/g, 'https://t.me/+rc5Psv_S2VJkMGM1')
+        .replace(/StudyUK/gi, 'NAKODA BHAIRAV');
 
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
-  res.send(html);
-};
+      res.setHeader('Content-Type', 'text/html');
+      res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
+      res.status(200).end(html);
+    });
+
+  }).on('error', err => {
+    console.error('Fetch failed:', err);
+    res.status(500).end('Proxy error!');
+  });
+}
